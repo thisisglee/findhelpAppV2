e8a5c43fafa8687186e54d053dad509dcab682f6
Gurleen Maggon <gurleenmaggon@gmail.com>
2021-07-08 15:19:52 -0400

    added footer/ header to show

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Image,Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, ScrollView, Linking } from 'react-native';
+import { Header, Icon, Button } from 'react-native-elements';
 
-import Logo from './assets/logo-findhelp.png'
+import Logo from './assets/logo-findhelp.png';
 
 export default function App() {
+  const operator = Platform.select({ ios: '&', android: '?' });
   return (
-    <View style={styles.container}>
-      {/* <Image source={require('./assets/logo-findhelp.png')} /> */}
-      <Image source={Logo} />
-      <Text>FindHelp</Text>
-      <Text>People Connected</Text>
-      <Text>Communities Strengthened</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Header
+        placement="left"
+        containerStyle={{
+          backgroundColor: '#0254a1',
+        }}
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+        rightComponent={{ icon: 'home', color: '#fff' }}
+      />
+      <ScrollView>
+        <View style={styles.container}>
+          {/* <Image source={require('./assets/logo-findhelp.png')} /> */}
+          <Image source={Logo} />
+          <Text>People Connected</Text>
+          <Text>Communities Strengthened</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+      <View style={{ height: 100, backgroundColor: '#ed3025' }}>
+        <Text style={{ textAlign: 'center', color: '#fff', fontSize: 20, marginTop: '2%' }}>
+          Connect 211?
+        </Text>
+        <View style={styles.item}>
+          <Button
+            onPress={() => {
+              Linking.openURL('tel:211');
+            }}
+            icon={<Icon name="call" color="#fff" />}
+            type="clear"
+          />
+          <Button
+            onPress={() => {
+              Linking.openURL(`sms:211`);
+            }}
+            icon={<Icon name="chat" color="#fff" />}
+            type="clear"
+          />
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -23,5 +58,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    // width: '45%',
+    // marginHorizontal: '10%',
+    // marginBottom: '5%',
   },
 });
